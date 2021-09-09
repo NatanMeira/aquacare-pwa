fc9c534364cf3df4cf07202e08f33aef10db8998
Natan Meira <natan.meira@redfox.tech>
2021-09-08 22:37:28 -0300

    chore: initital files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,21 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import GlobalStyles from '../src/styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white
+      },
+      {
+        name: 'blue',
+        value: theme.colors.mainBg
+      }
+    ]
+  },
   nextRouter: {
     Provider: RouterContext.Provider
   }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>AquaCare</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#0085FF" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Aplicativo que ajuda a cuidar da água do seu aquário"
         />
       </Head>
       <GlobalStyles />
