226b88346b748a09c05efe2cb86d831624724112
Natan Meira <natan.meira@redfox.tech>
2021-10-07 01:06:21 -0300

    feat: add sign in form

---
+++ src/components/Forms/SignIn/index.tsx
@@ -0,0 +1,72 @@
+import * as S from './styles'
+import { useFormik } from 'formik'
+import { toast } from 'react-toastify'
+import * as yup from 'yup'
+import InputText from 'components/InputText'
+import Button from 'components/Button'
+import { EmailOutline } from '@styled-icons/evaicons-outline/EmailOutline'
+import { LockPassword } from '@styled-icons/remix-line/LockPassword'
+
+const SignIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: SignInSchema,
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: async (values) => {
+      try {
+        // TODO: integrate with api
+        toast.success('Autenticado com sucesso!')
+      } catch (err) {
+        toast.error('Usuário ou senha incorretos!')
+      }
+    }
+  })
+
+  return (
+    <S.Form onSubmit={formik.handleSubmit}>
+      <S.FormControl>
+        <InputText
+          name="email"
+          type="email"
+          onChange={formik.handleChange('email')}
+          value={formik.values.email}
+          icon={<EmailOutline />}
+          onBlur={formik.handleBlur('email')}
+          error={formik.touched.email ? formik.errors.email : undefined}
+        />
+      </S.FormControl>
+
+      <S.FormControl>
+        <InputText
+          name="password"
+          type="password"
+          onChange={formik.handleChange('password')}
+          value={formik.values.password}
+          icon={<LockPassword />}
+          onBlur={formik.handleBlur('password')}
+          error={formik.touched.password ? formik.errors.password : undefined}
+        />
+      </S.FormControl>
+
+      <S.FormControl>
+        <Button type="submit" fullWidth>
+          Entrar
+        </Button>
+      </S.FormControl>
+    </S.Form>
+  )
+}
+
+const SignInSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Insira um e-mail válido')
+    .required('E-mail é um campo obrigatório'),
+  password: yup.string().required('Senha é um campo obrigatório')
+})
+
+export default SignIn
---
+++ src/components/Forms/SignIn/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Form = styled.form``
+export const FormControl = styled.div`
+  margin-top: 1.6rem;
+`
