b634727203574d5726977cbef5a7458db93ab0fa
Natan Meira <natan.meira@redfox.tech>
2021-10-07 02:50:12 -0300

    feat: add sign up page

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import Auth from 'templates/Auth'
+import FormSignUp from 'components/Forms/SignUp'
+
+export default function SignIn() {
+  return (
+    <Auth title="Registrar">
+      <FormSignUp />
+    </Auth>
+  )
+}
