4fb480285673636b64a3bcc6d2bf173e366bfb0e
Natan Meira <natan.meira@redfox.tech>
2021-10-08 22:26:57 -0300

    feat: set home as protected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { GetServerSidePropsContext } from 'next'
 import Home from 'templates/Home'
+import protectedRoutes from 'utils/protected-routes'
 
 export default function Index() {
   return <Home />
 }
- 
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+
+  return {
+    props: { session }
+  }
+}
